f277e4e585b1b0ea0de6c5f84fb67472e518aa34
khelifa yassin <yassin.khelifa@student.vinci.be>
2023-09-19 15:31:04 +0200

    1.1 API : read

---
+++ exercices/1.1/myMovies/routes/films.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const films = [
+  {
+    id : 1, title : "SpiderMan", duration : 120, budget : 200, link : "hviurnvjrojo"
+  },
+  {
+    id : 2, title : "BatMan", duration : 120, budget : 200, link : "hviurnvjrojo"
+  },
+  {
+    id : 3, title : "IronMan", duration : 120, budget : 200, link : "hviurnvjrojo"
+  }
+]
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json(films);
+});
+
+module.exports = router;
