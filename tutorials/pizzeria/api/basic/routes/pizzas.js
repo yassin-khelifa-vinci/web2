3c340b9107a559de4ad9ad2cba9f11b198540bd8
khelifa yassin <yassin.khelifa@student.vinci.be>
2023-09-25 12:05:31 +0200

    tutorial REST Client

--- tutorials/pizzeria/api/basic/routes/pizzas.js
+++ tutorials/pizzeria/api/basic/routes/pizzas.js
@@ -29,10 +29,47 @@ const MENU = [
   },
 ];
 
-// Read all the pizzas from the menu
+/* Read all the pizzas from the menu
+   GET /pizzas?order=title : ascending order by title
+   GET /pizzas?order=-title : descending order by title
+*/
 router.get('/', (req, res, next) => {
+  const orderByTitle =
+    req?.query?.order?.includes('title') ? req.query.order : undefined;
+  let orderedMenu;
+  console.log(`order by ${orderByTitle ?? 'not requested'}`);
+  if (orderByTitle)
+    orderedMenu = [...MENU].sort((a, b) => a.title.localeCompare(b.title));
+  if (orderByTitle === '-title') orderedMenu = orderedMenu.reverse();
+
   console.log('GET /pizzas');
-  res.json(MENU);
+  res.json(orderedMenu ?? MENU);
 });
+
+// Create a pizza to be added to the menu.
+router.post('/', (req, res) => {
+  const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+  const content = req?.body?.content?.length !== 0 ? req.body.content : undefined;
+
+  console.log('POST /pizzas');
+
+  if (!title || !content) return res.sendStatus(400); // error code '400 Bad request'
+
+  const lastItemIndex = MENU?.length !== 0 ? MENU.length - 1 : undefined;
+  const lastId = lastItemIndex !== undefined ? MENU[lastItemIndex]?.id : 0;
+  const nextId = lastId + 1;
+
+  const newPizza = {
+    id: nextId,
+    title: title,
+    content: content,
+  };
+
+  MENU.push(newPizza);
+
+  res.json(newPizza);
+});
+
+
 
 module.exports = router;
