4da7dacd8a580f883807bd6278e2d24c5b0215f4
khelifa yassin <yassin.khelifa@student.vinci.be>
2023-10-02 12:22:53 +0200

    1.7 : API : persistence

---
+++ exercices/1.7/routes/films.js
@@ -0,0 +1,160 @@
+var express = require('express');
+const { serialize, parse } = require('../utils/json');
+var router = express.Router();
+
+const jsonDbPath = __dirname + '/../data/films.json';
+
+const FILMS = [
+  {
+    id : 1, title : "SpiderMan", duration : 120, budget : 200, link : "hviurnvjrojo"
+  },
+  {
+    id : 2, title : "BatMan", duration : 150, budget : 200, link : "hviurnvjrojo"
+  },
+  {
+    id : 3, title : "Hulk", duration : 180, budget : 200, link : "hviurnvjrojo"
+  }
+]
+
+// Read the film identified by an id in the films table
+router.get('/:id', (req, res) => {
+  console.log(`GET /films/${req.params.id}`);
+
+  if (isNaN(parseInt(req.params.id))) return res.sendStatus(400);
+
+  const films = parse(jsonDbPath, FILMS);
+
+  const indexOfFilmFound = films.findIndex((film) => film.id == req.params.id);
+
+  if (indexOfFilmFound < 0) return res.sendStatus(404);
+
+  res.json(films[indexOfFilmFound]);
+});
+
+router.get('/', (req, res, next) => {
+  const filter = parseInt(req?.query?.["minimum-duration"]);
+  console.log(filter);
+  const filteredByDuration =
+    !isNaN(filter) ? filter : undefined;
+  let filteredFilms;
+  const films = parse(jsonDbPath, FILMS);
+  if (filteredByDuration)
+    filteredFilms = [];
+    for (const film of films) {
+      if (film.duration >= filteredByDuration) filteredFilms.push(film); 
+    }
+  res.json(filteredFilms ?? films)
+});
+
+router.post('/', (req, res) => {
+  const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+  const duration = req?.body?.duration >= 0 ? req.body.duration : undefined;
+  const budget = req?.body?.budget >= 0 ? req.body.budget : undefined;
+  const link = req?.body?.link?.length !== 0 ? req.body.link : undefined;
+
+  console.log('POST /films');
+
+  if (!title || !duration || !budget || !link) return res.sendStatus(400); // error code '400 Bad request'
+
+  const films = parse(jsonDbPath, FILMS);
+
+  if (films.findIndex((film) => film.title === title) >= 0) return res.sendStatus(409);
+
+  const lastItemIndex = films?.length !== 0 ? films.length - 1 : undefined;
+  const lastId = lastItemIndex !== undefined ? films[lastItemIndex].id : 0;
+  const nextId = lastId + 1;
+
+  const newFilm = {
+    id: nextId,
+    title: title,
+    duration: duration,
+    budget: budget,
+    link: link
+  };
+
+  films.push(newFilm);
+  serialize(jsonDbPath, films)
+
+  res.json(newFilm);
+});
+
+router.delete('/:id', (req, res) => {
+
+  if (isNaN(parseInt(req.params.id))) return res.sendStatus(400);
+
+  const films = parse(jsonDbPath, FILMS);
+
+  const indexFilm = films.findIndex((film) => film.id == req.params.id);
+  if (indexFilm < 0) return res.sendStatus(404);
+  const itemRemoved = films.splice(indexFilm, 1);
+
+  serialize(jsonDbPath, films);
+
+  res.json(itemRemoved[0])
+});
+
+router.patch('/:id', (req, res) => {
+  if (isNaN(parseInt(req.params.id))) return res.sendStatus(400);
+
+  const title = req?.body?.title;
+  const duration = req?.body?.duration;
+  const budget = req?.body?.budget;
+  const link = req?.body?.link;
+
+  console.log(title?.length === 0)
+
+  if ((!title && !duration && !budget && !link) || title?.length === 0 || duration <= 0 || budget <= 0 || link?.length === 0) return res.sendStatus(400);
+
+  const films = parse(jsonDbPath, FILMS);
+  const foundIndex = films.findIndex((film) => film.id == req.params.id);
+
+  if (foundIndex < 0) return res.sendStatus(404);
+
+  const itemUpdated = {...films[foundIndex], ...req.body};
+  films[foundIndex] = itemUpdated;
+
+  serialize(jsonDbPath, films);
+
+  res.json(itemUpdated)
+});
+
+router.put('/:id', (req, res) => {
+  if (isNaN(parseInt(req.params.id))) return res.sendStatus(400);
+
+  const title = req?.body?.title;
+  const duration = req?.body?.duration;
+  const budget = req?.body?.budget;
+  const link = req?.body?.link;
+
+  console.log(title?.length === 0)
+
+  if ((!title && !duration && !budget && !link) || title === undefined || title?.length === 0 || duration === undefined || duration <= 0 || budget === undefined || budget <= 0 || link === undefined || link?.length === 0) return res.sendStatus(400);
+
+  const films = parse(jsonDbPath, FILMS);
+  const foundIndex = films.findIndex((film) => film.id == req.params.id);
+
+  let item;
+
+  console.log(films)
+
+  if (foundIndex < 0){
+    item = {
+      id: films.length !== 0 ? films[films.length - 1].id + 1 : 1,
+      title: title,
+      duration: duration,
+      budget: budget,
+      link: link
+    };
+    films.push(item)
+  } 
+  else{
+  item = {...films[foundIndex], ...req.body};
+  films[foundIndex] = item;
+  }
+
+  serialize(jsonDbPath, films);
+
+  res.json(item)
+});
+
+module.exports = router;
