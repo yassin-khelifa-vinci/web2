ca6ec88238a8104c5b08f6602897903a7e32957a
khelifa yassin <yassin.khelifa@student.vinci.be>
2023-11-06 22:00:48 +0100

    structured-hmi tutorial

---
+++ tutorials/pizzeria/hmi/structured/src/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+import 'animate.css';
+
+import HomePage from './Components/Pages/HomePage';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import NavBar from './Components/NavBar/NavBar';
+
+Header();
+NavBar();
+HomePage();
+Footer();
